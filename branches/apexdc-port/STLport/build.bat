00b89d8c08ce5d394160271e5d6bbfcb6831e871
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-01-26 13:27:46 +0000

    multithreaded stlport lib

--- branches/apexdc-port/STLport/build.bat
+++ branches/apexdc-port/STLport/build.bat
@@ -1,5 +1,7 @@
 @echo off
 del build\lib\obj /S /Q
+rem multithreaded  version
+call configure msvc9 --rtl-dynamic
 cd build\lib
 call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 nmake clean install
