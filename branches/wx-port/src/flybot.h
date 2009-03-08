2e219998285e7d46c0a501aa383f93035a404517
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-03-08 18:13:34 +0000

    wx branch: initial import

---
+++ branches/wx-port/src/flybot.h
@@ -0,0 +1,34 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the FLYBOT_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// FLYBOT_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#include "wx/wx.h"
+#include <windows.h>
+#include <process.h>
+
+#include "ChatBotAPI.h"
+#include "tray.h"
+
+
+//#ifdef FLYBOT_EXPORTS
+#define FLYBOT_API __declspec(dllexport)
+//#else
+//#define FLYBOT_API __declspec(dllimport)
+//#endif
+
+
+struct BotInit _init;
+CRITICAL_SECTION logcs;
+HANDLE ThreadId;
+
+
+extern "C" FLYBOT_API bool __stdcall init(BotInit*);
+
+class wxDLLApp : public wxApp
+{
+	MyTaskBarIcon   *m_taskBarIcon;
+	bool OnInit();
+	virtual ~wxDLLApp();
+};
