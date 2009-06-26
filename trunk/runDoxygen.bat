1b52ebed90cade8bfdc5a6398886b180f20596af
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-06-26 13:00:00 +0000

    wiped out delphi-based trunk

--- trunk/runDoxygen.bat
+++
@@ -1,6 +0,0 @@
-mkdir .\help
-mkdir .\help\html
-del /S /Q .\help\html\*
-"D:\Program Files\doxygen\bin\doxygen.exe" flybot.doxygen
-"D:\Program Files\HTML Help Workshop\hhc.exe" .\help\html\index.hhp
-pause
