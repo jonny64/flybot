c71834c54f4577151b80a30789e0b97cab15b2c0
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-09-17 16:38:15 +0000

    64-bit installer

--- scripts/post-build-release-x86_64.bat
+++ scripts/post-build-release-x86_64.bat
@@ -5,21 +5,24 @@ SubWCRev.exe ..\ verpatch.src verpatch.bat
 set platformName = %1
 call verpatch.bat platformName
 
+echo generating packaging script...
+echo !define VERSION %VERSION% > flybot.nsi
+echo OutFile ..\release\${VERSION}\flybot-${VERSION}-x64.exe >> flybot.nsi
+more flybot.x86_64.src >> flybot.nsi
+
 echo packaging...
-rem if not exist "%PROGRAMFILES%\NSIS\makensis.exe" GOTO NONSIS
-rem "%PROGRAMFILES%\NSIS\makensis" /v2 flybot.nsi
-rem TODO: http://nsis.download3000.com/nsis-2.26
-rem below is temporary solution
-set botDir=..\setup\flybot-%VERSION%-x86_64
-mkdir "%botDir%\Settings"
-copy /y "..\bin\Chatbot.dll" "%botDir%"
-copy /y "..\bin\Chatbot.mo" "%botDir%"
-copy /y "..\Translation\Russian\flydict.ini" "%botDir%\Settings"
-"c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\flybot-%VERSION%-x86_64.zip" %botDir%
+if not exist "%PROGRAMFILES%\NSIS\makensis.exe" GOTO NONSIS
+"%PROGRAMFILES%\NSIS\makensis" /v2 flybot.nsi
+
+rem set botDir=..\setup\flybot-%VERSION%-x86_64
+rem mkdir "%botDir%\Settings"
+rem copy /y "..\bin\Chatbot.dll" "%botDir%"
+rem copy /y "..\bin\Chatbot.mo" "%botDir%"
+rem copy /y "..\Translation\Russian\flydict.ini" "%botDir%\Settings"
+rem "c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\flybot-%VERSION%-x86_64.zip" %botDir%
 
 if not exist "..\release\%VERSION%" mkdir "..\release\%VERSION%"
-copy "..\bin\Chatbot.pdb" "..\release\%VERSION%\Chatbot.%VERSION%.x86_64.pdb"
-
+"c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\Chatbot.%VERSION%.x86_64.pdb.zip" "..\bin\Chatbot.pdb"
 pause
 exit
 
--- scripts/post-build-release.bat
+++ scripts/post-build-release.bat
@@ -6,12 +6,17 @@ call verpatch.bat platformName
 
 copy /y "..\Translation\Russian\*.mo" "..\bin"
 
+echo generating packaging script...
+echo !define VERSION %VERSION% > flybot.nsi
+echo OutFile ..\release\${VERSION}\flybot-${VERSION}.exe >> flybot.nsi
+more flybot.src >> flybot.nsi
+
 echo packaging...
 if not exist "%PROGRAMFILES%\NSIS\makensis.exe" GOTO NONSIS
 "%PROGRAMFILES%\NSIS\makensis" /v2 flybot.nsi
 
 if not exist "..\release\%VERSION%" mkdir "..\release\%VERSION%"
-copy "..\bin\Chatbot.pdb" "..\release\%VERSION%\Chatbot.%VERSION%.pdb"
+"c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\Chatbot.%VERSION%.pdb.zip" "..\bin\Chatbot.pdb"
 
 pause
 exit
