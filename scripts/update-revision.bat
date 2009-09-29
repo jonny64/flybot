43d0601dea4983c2b75f74dde29b7e38ea9e151d
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-09-29 18:22:31 +0000

    fixed () in update script

--- scripts/update-revision.bat
+++ scripts/update-revision.bat
@@ -1,6 +1,5 @@
 for /f "tokens=1,2 delims= " %%a in ('svn.exe st -q ..') do (
-  if "%%a"=="M" (
+  if "%%a"=="M"
     echo update-revision.bat : Deploy error E0001 : "%%b" changed. Commit changes before deploy!
-    exit /b 1
-  )
+  if "%%a"=="M" exit /b 1
 )
