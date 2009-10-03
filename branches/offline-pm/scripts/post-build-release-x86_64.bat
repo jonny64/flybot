2b9e59f58f27320ac32fe1e41fa90ed69126c982
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-03 14:46:31 +0000

    OfflinePM test branch

---
+++ branches/offline-pm/scripts/post-build-release-x86_64.bat
@@ -0,0 +1,42 @@
+@echo off
+set PATH=..\tools;%PATH%
+call "..\scripts\update-revision.bat"
+if errorlevel 1 GOTO :END
+
+cd "..\setup"
+SubWCRev.exe ..\ verpatch.src verpatch.bat
+set platformName = %1
+call verpatch.bat platformName
+
+echo generating packaging script...
+echo !define VERSION %VERSION% > flybot.nsi
+echo OutFile ..\release\${VERSION}\flybot-${VERSION}-x64.exe >> flybot.nsi
+more flybot.x86_64.src >> flybot.nsi
+
+if not exist "..\release\%VERSION%" mkdir "..\release\%VERSION%"
+
+echo packaging...
+if not exist "%PROGRAMFILES%\NSIS\makensis.exe" GOTO :NONSIS
+"%PROGRAMFILES%\NSIS\makensis" /v2 flybot.nsi
+
+rem set botDir=..\setup\flybot-%VERSION%-x86_64
+rem mkdir "%botDir%\Settings"
+rem copy /y "..\bin\Chatbot.dll" "%botDir%"
+rem copy /y "..\bin\Chatbot.mo" "%botDir%"
+rem copy /y "..\Translation\Russian\flydict.ini" "%botDir%\Settings"
+rem "c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\flybot-%VERSION%-x86_64.zip" %botDir%
+
+"c:\Program Files\7-Zip\7z" a -tzip "..\release\%VERSION%\Chatbot.%VERSION%.x86_64.pdb.zip" "..\bin\Chatbot.pdb"
+
+:END
+pause
+exit
+
+:NONSIS
+echo post-build-release-x86_64.bat : Tools error E0002 : You don't have NSIS installed. Aborting.
+pause
+exit -2
+
+:ERROR
+pause
+exit %errorlevel%
---
+++ branches/offline-pm/source/stdwx.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <wx/wxprec.h>
+#include <wx/msw/private.h>
+#include <wx/thread.h>
+#include <wx/wx.h>
+#include <wx/tokenzr.h>
+#include <wx/taskbar.h>
+#include <wx/config.h>
+
+
+// memory allocation enhancement
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK ,__FILE__, __LINE__)
+#else
+#define DEBUG_NEW new
+#endif
+
+const int SUCCESS = 0;
+
+// TODO: remove after wxWidgets 2.9 release
+// http://trac.wxwidgets.org/ticket/8370
+wxArrayString wxSplit(const wxString& str, const wxChar sep, const wxChar escape);
+
+// TODO: remove as soon as wxWidgets 2.9 released 
+// http://trac.wxwidgets.org/ticket/9810
+bool wxLaunchDefaultApplication(const wxString &document);
+
+int random(int max);
+
+
+#define FOREACH(type, it, container) \
+    for (type::iterator (it) = (container).begin(); (it) != (container).end(); ++(it))
+
+#define FOREACH_CONST(type, it, container) \
+    for (type::const_iterator (it) = (container).begin(); (it) != (container).end(); ++(it))
