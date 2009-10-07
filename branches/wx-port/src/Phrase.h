2a4393e6e829bca4ba4848c87a20ba34eedc4a00
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-07 17:13:53 +0000

    dropped obsolete branches

--- branches/wx-port/src/Phrase.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Phrase
-{
-	int Priority;
-	wxString MatchExpr;
-	wxString Answer;
-	wxString Flags;
-};
--- branches/wx-widgets-2.9.0-based/scripts/get-version-string.bat
+++
@@ -1,2 +0,0 @@
-SubWCRev.exe ..\ bot-version.template bot-version.bat
-call bot-version.bat
