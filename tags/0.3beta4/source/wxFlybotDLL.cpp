15f80509642bb04ddb10942123dd5591e10f15ab
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-14 12:45:56 +0000

    copied beta 4 tag

---
+++ tags/0.3beta4/source/wxFlybotDLL.cpp
@@ -0,0 +1,126 @@
+#include "stdwx.h"
+#include <wx/stdpaths.h>
+#include <wx/utils.h>
+#include "wxFlybotDLL.h"
+#include "wxLogBalloon.h"
+#include "FlybotConfig.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+IMPLEMENT_APP_NO_MAIN(wxFlybotDLL)
+
+wxCriticalSection gSession;
+
+bool wxFlybotDLL::GetEnabledState()
+{
+    return m_online;
+}
+
+void wxFlybotDLL::SwitchState()
+{
+    m_online = !m_online;
+}
+
+
+bool wxFlybotDLL::OnInit()
+{
+    m_taskBarIcon = NULL;
+    m_locale = NULL;
+    
+    Config.Read(SETTING_BOT_ONLINE, &m_online, true);
+        
+    SelectLanguage(wxLANGUAGE_RUSSIAN);
+
+    m_taskBarIcon = new FlybotTaskBarIcon();
+    if (!m_taskBarIcon)
+    {
+        return false;
+    }
+    // set new logger (SetActiveTarget returns old logger)
+    delete wxLog::SetActiveTarget(new wxLogBalloon(m_taskBarIcon));
+
+    Dict.Load();
+    return true;
+}
+
+void wxFlybotDLL::SelectLanguage(int lang)
+{
+    delete m_locale;
+    m_locale = new wxLocale(lang);
+    if (!m_locale)
+    {
+        return;
+    }
+
+    m_locale->AddCatalogLookupPathPrefix(wxStandardPaths::Get().GetPluginsDir());
+    if (!m_locale->AddCatalog(wxT("Chatbot")) )
+    {
+        // fall back to embedded english (do nothing)
+    }
+}
+
+void wxFlybotDLL::ReloadDictionary()
+{
+    if (SUCCESS == Dict.Load())
+    {
+        wxLogMessage(_("Dictionary was successfully loaded"));
+    }
+}
+
+void wxFlybotDLL::OpenDictionary()
+{
+    if (!wxLaunchDefaultApplication(Dictionary::GetDictionaryFilename()) )
+    {
+        // TODO: handle case when no program associated to .ini files
+    }
+}
+
+void wxFlybotDLL::HandlePM(UserInfo& userinfo, wxString& msg)
+{
+    // do not process favourites
+    if ( userinfo.Favourite() )
+        return;
+
+    wxCriticalSectionLocker locker(gSession); // leaves cs in destructor
+    
+    // if it is a new PM, create new session
+    wxString cid = userinfo[FLYBOT_API_CID];
+    if (NULL == m_sessions[cid])
+    {
+        m_sessions[cid] = new Session(userinfo);
+    }
+
+    // answer pm, according to previous replies, etc.
+    if (NULL != m_sessions[cid])
+    {
+        m_sessions[cid]->Answer(msg);
+    }
+}
+
+int wxFlybotDLL::OnExit()
+{
+    Config.Write(SETTING_BOT_ONLINE, m_online);
+
+    if (m_taskBarIcon)
+    {
+        m_taskBarIcon->RemoveIcon();
+        delete m_taskBarIcon;
+    }
+
+    // free session info
+    FOREACH(SessionMap, it, m_sessions)
+    {
+        delete it->second;
+    }
+    m_sessions.clear();
+
+    delete m_locale;
+
+    return 0;
+}
+
+wxFlybotDLL::~wxFlybotDLL()
+{
+}
---
+++ tags/0.3beta4/source/wxLogBalloon.cpp
@@ -0,0 +1,93 @@
+#include "stdwx.h"
+#include "wxLogBalloon.h"
+#include "wxFlybotDLL.h"
+
+wxLogBalloon::wxLogBalloon(FlybotTaskBarIcon *tb)
+{
+    m_taskBarIcon = tb;
+}
+
+void wxLogBalloon::DoLogString(const wxString& message, time_t WXUNUSED(t), int icon)
+{
+    if ( message.empty())
+        return;
+
+    // message title and body are separated by special char
+    wxArrayString tokens = wxSplit(message, BALLOON_LOGGER_SEPARATOR_CHAR, BALLOON_LOGGER_ESCAPE_CHAR);
+    wxASSERT( tokens.Count() > 0 );
+
+    // if no title specified use default one
+    wxString balloonText = tokens[0];
+    wxString balloonTitle = tokens.Count() > 1? tokens[1] : wxT("flybot");
+
+    if (NULL != m_taskBarIcon)
+    {
+        m_taskBarIcon->ShowBalloon(balloonTitle, balloonText, LOG_BALLOON_TIMEOUT_MS, icon);
+    }
+}
+
+#define IMPLEMENT_BALLOON_LOG_FUNCTION(level)                                                                                     \
+void wxLog##level(const wxString &title, const wxString &message)\
+{                                                                \
+    wxString msg  = wxString::Format(                            \
+        wxT("%s%c%s"),                                           \
+        message,                                                 \
+        BALLOON_LOGGER_SEPARATOR_CHAR,                           \
+        title                                                    \
+    );                                                           \
+    /* use default implementation, which later calls DoLog */    \
+    wxLog##level(msg);                                           \
+}
+
+
+IMPLEMENT_BALLOON_LOG_FUNCTION(Error)
+IMPLEMENT_BALLOON_LOG_FUNCTION(Warning)
+IMPLEMENT_BALLOON_LOG_FUNCTION(Message)
+IMPLEMENT_BALLOON_LOG_FUNCTION(Info)
+IMPLEMENT_BALLOON_LOG_FUNCTION(Status)
+
+void wxLogBalloon::DoLog(wxLogLevel level, const wxString& szString, time_t t)
+{
+    switch ( level ) 
+    {
+    case wxLOG_FatalError:
+        DoLogString(_("Fatal error: ") + szString + _("Program aborted."), t, wxICON_ERROR);
+        Flush();
+        abort();
+        break;
+
+    case wxLOG_Error:
+        DoLogString(szString, t, wxICON_ERROR);
+        break;
+
+    case wxLOG_Warning:
+        DoLogString(szString, t, wxICON_WARNING);
+        break;
+
+    case wxLOG_Info:
+        if ( GetVerbose() )
+    case wxLOG_Message:
+    case wxLOG_Status:
+    default:    // log unknown log levels too
+        if (wxGetApp().Config.BalloonsEnabled())
+            DoLogString(szString, t);
+        break;
+
+    case wxLOG_Trace:
+    case wxLOG_Debug:
+#ifdef __WXDEBUG__
+        {
+            wxString msg = level == wxLOG_Trace ? wxT("Trace: ")
+                : wxT("Debug: ");
+            msg << szString;
+            DoLogString(msg, t);
+        }
+#endif // Debug
+        break;
+    }
+}
+
+wxLogBalloon::~wxLogBalloon(void)
+{
+}
+
