1c2bba31bb092eaa0be4cff465af8e075a62ceeb
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-14 12:42:42 +0000

    0.30 release

---
+++ tags/0.30/scripts/workspace-config.bat
@@ -0,0 +1,25 @@
+rem @echo off
+set PATH=..\tools;%PATH%
+
+rem cd to bat file directory: need when run from explorer
+cd /d %~dp0
+set wxWidgetsDir=e:\Code\wxWidgets-2.9.0
+set currDir=.
+
+copy /y "%wxWidgetsDir%\include\wx\msw\setup.h" "%wxWidgetsDir%\include\wx\msw\setup.h.bak"
+copy /y setup.h "%wxWidgetsDir%\include\wx\msw\setup.h"
+
+mklink /d "..\include" "%wxWidgetsDir%\include"
+
+rem wxWidgets library project (build\msw\wx.dsw) also should be modified
+rem see http://wiki.wxwidgets.org/Supporting_x64_and_Win32_within_one_solution
+rem or you can apply Scripts\tag-2.9.0-multiplatform-support.patch
+mklink /d "..\lib" "%wxWidgetsDir%\lib"
+
+rem cd %wxWidgetsDir%
+rem call %currDir%\VC2005_MultiTargetSupport.bat
+
+mkdir "e:\tmp\My Dropbox\Public\flybot"
+mklink /d "..\release" "e:\tmp\My Dropbox\Public\flybot"
+
+pause
