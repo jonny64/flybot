937ddef549ec4d29ff8d3dce0270d5b32d533781
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-14 12:50:53 +0000

    removed obsolete branches

--- branches/apexdc-port/STLport/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-del build\lib\obj /S /Q
-rem multithreaded  version
-call configure msvc9 --rtl-dynamic
-cd build\lib
-call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-nmake clean install
--- tag/0.3beta4/scripts/get-version-string.bat
+++
@@ -1,7 +0,0 @@
-rem setlocal EnableExtensions EnableDelayedExpansion
-set PATH=..\tools;%PATH%
-
-set VERSION=
-verpatch ..\bin\Chatbot.dll /vo /xi | gawk "/ProductVersion/ { print gensub(/\.0/, \"\", 1, $3) }" > version.txt
-for /f "usebackq delims=" %%g in ("version.txt") do set VERSION=%%g
-set VERSION=%VERSION:"=%-beta4
