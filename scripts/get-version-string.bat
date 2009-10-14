4276ff382c211d5460fb3ebb866e221840acb147
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2009-10-14 12:45:56 +0000

    copied beta 4 tag

--- scripts/get-version-string.bat
+++ scripts/get-version-string.bat
@@ -4,4 +4,4 @@ set PATH=..\tools;%PATH%
 set VERSION=
 verpatch ..\bin\Chatbot.dll /vo /xi | gawk "/ProductVersion/ { print gensub(/\.0/, \"\", 1, $3) }" > version.txt
 for /f "usebackq delims=" %%g in ("version.txt") do set VERSION=%%g
-set VERSION=%VERSION:"=%
+set VERSION=%VERSION:"=%-beta4
