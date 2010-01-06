485513519f5bb26741f42fa503952357f94f71ea
astro64m <astro64m@ce89f4f8-5c4d-0410-bcc5-036a2582dbce>
2010-01-06 12:21:32 +0000

    library classes to monitor dictionary changes

--- source/DictionaryFSWatcher.cpp
+++ source/DictionaryFSWatcher.cpp
@@ -13,19 +13,16 @@ DictionaryFSWatcher::~DictionaryFSWatcher(void)
 {
 }
 
-
+// checks dictionary modification time and reloads it when needed
 void DictionaryFSWatcher::Notify()
 {
-	struct	__stat64 fileinfo;
-	wxString dictionaryFullPath = FlybotAPI.ConfigPath + DICTIONARY_FILENAME;
-	
-	if (0 != _stat64(dictionaryFullPath.mb_str(wxConvUTF8), &fileinfo) ) 
-        return;
-
-    static __time64_t oldTimestamp = fileinfo.st_mtime; // persistent between calls
-	if (fileinfo.st_mtime != oldTimestamp)              // dictionary file changed by user
+	wxFileName dictionary(FlybotAPI.ConfigPath + DICTIONARY_FILENAME);
+    wxDateTime mtime = dictionary.GetModificationTime();
+    
+    static wxDateTime oldTimestamp = mtime;
+	if ( oldTimestamp != mtime )
 	{
 		wxGetApp().ReloadDictionary();
-		oldTimestamp = fileinfo.st_mtime;
+		oldTimestamp = mtime;
 	}
 }
